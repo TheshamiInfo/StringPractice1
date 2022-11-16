fdf913f599027695fc651cff914b4c1ffae37b84
Theshami <Theshami@DESKTOP>
2022-11-16 23:11:58 +0530

    Commit by @orther /TheShamiInfo

---
+++ src/Add/AddTwoWord.java
@@ -0,0 +1,16 @@
+package Add;
+public class AddTwoWord {
+
+	public static void main(String[] args) {
+		
+		String str1 = "manish";
+		String str2  = "sir";
+		
+		con( str1,str2);
+	}
+		static void con(String str1,String str2) {
+			System.out.println(str1.concat(str2));
+			
+			
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Theshami
+ *
+ */
+module StringPractice1 {
+}
